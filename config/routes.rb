08e350d36b23180315f87cf7579006be9ae7c35c
NakanishiShoko <129033329+nakashoo@users.noreply.github.com>
2023-12-16 14:02:00 +0000

    <add>resources:books追記

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  get 'homes/top'
   devise_for :users
   root to: 'homes#top'
   get 'homes/about', to: 'homes#about', as: 'about'
+
+  resources :books
+
 end
