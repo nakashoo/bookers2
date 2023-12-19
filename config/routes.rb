e4e5b1bddcb1683e2d5c77402a3b8e2a96e5c193
NakanishiShoko <129033329+nakashoo@users.noreply.github.com>
2023-12-19 01:55:57 +0000

    <add>users、bookのresourcesとpatchを記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-
   devise_for :users
   root to: 'homes#top'
+
+  resources :users
+  resources :books, except: [:update]
+  patch 'books/:id/update', to: 'books#update', as: 'update_book'
   get 'homes/about', to: 'homes#about', as: 'about'
 
-  resources :books
-  resources :users, only: [:show, :edit]
+
 end
