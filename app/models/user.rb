2f49ffc1cf6976807012f09202a5d76b75ca2a6b
NakanishiShoko <129033329+nakashoo@users.noreply.github.com>
2023-12-19 02:03:10 +0000

    <add>profile_imageを使える記述とintroductionのバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,24 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :book, dependent: :destroy
+
+    has_one_attached :profile_image
+    validates :introduction, presence: true
+
+  def get_profile_image
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/default-image.jpeg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpeg', content_type: 'image/jpeg')
+    end
+    profile_image
+  end
+
 end
+
+# <% @users.each do |user| %>
+#             <tr>
+#               <td><%= image_tag user.get_profile_image, size: "75x75" %></td>
+#               <td><%= user.name %></td>
+#               <td><%= link_to "Show", user_path(user.id) %></td>
+#             </tr>
+#           <% end %>
