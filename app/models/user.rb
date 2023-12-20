08c73320e080636fe77df8716218fcfaef75c0c8
NakanishiShoko <129033329+nakashoo@users.noreply.github.com>
2023-12-20 13:16:26 +0000

    <add>introductionを削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,9 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :books, dependent: :destroy
 
     has_one_attached :profile_image
-    validates :introduction, presence: true
 
   def get_profile_image
     unless profile_image.attached?
@@ -17,11 +14,3 @@ class User < ApplicationRecord
   end
 
 end
-
-# <% @users.each do |user| %>
-#             <tr>
-#               <td><%= image_tag user.get_profile_image, size: "75x75" %></td>
-#               <td><%= user.name %></td>
-#               <td><%= link_to "Show", user_path(user.id) %></td>
-#             </tr>
-#           <% end %>
