c193ca51f88d8a1899582cd167b77ac88dae433b
NakanishiShoko <129033329+nakashoo@users.noreply.github.com>
2023-12-21 12:17:39 +0000

    <add>resources book以降の記述を削除

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,7 @@ Rails.application.routes.draw do
   root to: 'homes#top'
 
   resources :users
-  resources :books, except: [:update]
-  patch 'books/:id/update', to: 'books#update', as: 'update_book'
+  resources :books
   get 'homes/about', to: 'homes#about', as: 'about'
-
 
 end
