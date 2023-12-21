e40ac216cdddf647d316bcd5dbcfe42ecb913c8b
NakanishiShoko <129033329+nakashoo@users.noreply.github.com>
2023-12-21 23:26:00 +0000

    <add>ログアウト時のフラッシュメッセージ設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
 
 
   def after_sign_out_path_for(resource)
-    about_path
+    root_path
   end
 
   protected
@@ -15,4 +15,12 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
+
+
+  def after_sign_out_path_for(resource_or_scope)
+    flash[:notice] = "Signed out successfully."
+    super
+  end
+
+
 end
