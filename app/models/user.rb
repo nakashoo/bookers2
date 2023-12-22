835d5d83f9b33b0fe831984e96430682579d115a
NakanishiShoko <129033329+nakashoo@users.noreply.github.com>
2023-12-22 10:06:04 +0000

    <add>userのバリデーション追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   has_many :books, dependent: :destroy
 
-    has_one_attached :profile_image
+  has_one_attached :profile_image
 
   def get_profile_image
     unless profile_image.attached?
@@ -11,6 +12,18 @@ class User < ApplicationRecord
       profile_image.attach(io: File.open(file_path), filename: 'default-image.jpeg', content_type: 'image/jpeg')
     end
     profile_image
+  end
+
+  validate :custom_name_validation
+
+  private
+
+  def custom_name_validation
+    if name.blank?
+      errors.add(:name, "can't be blank")
+    elsif name.length < 2
+      errors.add(:name, "is too short (minimum is 2 characters)")
+    end
   end
 
 end
