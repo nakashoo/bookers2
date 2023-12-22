9752efd1cec4f1fac56956afbdde28e038d81553
NakanishiShoko <129033329+nakashoo@users.noreply.github.com>
2023-12-22 01:20:22 +0000

    <add>プロフィール更新時のフラッシュメッセージ記述

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   def index
     @users = User.all
     @user = User.new
+    @book = Book.new
   end
 
 
@@ -19,8 +20,10 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to user_path(@user.id)
+    if @user.update(user_params)
+      flash[:notice] = "You have updated user successfully."
+      redirect_to user_path(@user.id)
+    end
   end
 
   private
