89df4b521c1b26365f0049f89dbe8d0199f85e12
NakanishiShoko <129033329+nakashoo@users.noreply.github.com>
2023-12-22 16:45:34 +0000

    <add>destroyの文言消去、createで@useを定義

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,14 +6,17 @@ class BooksController < ApplicationController
     @user = current_user
   end
 
-
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
+    @user = current_user
 
     if @book.save
       flash[:notice] = "You have created book successfully."
       redirect_to book_path(@book)
+    else
+      @errors = @book.errors.full_messages
+      render :index
     end
   end
 
@@ -40,7 +43,7 @@ class BooksController < ApplicationController
   def destroy
     @book = Book.find(params[:id])
     if @book.destroy
-    redirect_to books_path, notice: '削除しました'
+    redirect_to books_path
     end
   end
 
