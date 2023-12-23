0a8394a8d16986f2eb5d9ad86602b74a2c0331fe
NakanishiShoko <129033329+nakashoo@users.noreply.github.com>
2023-12-23 07:06:23 +0000

    <add>ログアウトの記述変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-  end
-
-
   def after_sign_in_path_for(resource)
     flash[:notice] = "Welcome! You have signed up successfully."
     user_path(current_user)
@@ -19,5 +11,10 @@ class ApplicationController < ActionController::Base
     super
   end
 
+
+protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+  end
 
 end
