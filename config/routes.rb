45a445bcddbf33a640c988fd54e5a0772aeb627d
NakanishiShoko <129033329+nakashoo@users.noreply.github.com>
2023-12-24 05:53:27 +0000

    <add>homes/aboutをhome/aboutに修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
+  get '/home/about', to: 'homes#about', as: 'about'
 
   resources :users
   resources :books
-  get 'homes/about', to: 'homes#about', as: 'about'
+   #get 'homes/about', to: 'homes#about', as: 'about'
 
 end
