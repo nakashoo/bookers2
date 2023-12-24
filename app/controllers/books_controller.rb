a9746d7e0a1498c4b443d9e9de4e2606e600d511
NakanishiShoko <129033329+nakashoo@users.noreply.github.com>
2023-12-24 02:21:46 +0000

    <add>bookモデルのopinionをbodyに変更

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -52,7 +52,7 @@ class BooksController < ApplicationController
 
   private
   def book_params
-    params.require(:book).permit(:title,:opinion)
+    params.require(:book).permit(:title,:body)
   end
 
 
--- app/models/book.rb
+++ app/models/book.rb
@@ -3,6 +3,6 @@ class Book < ApplicationRecord
   belongs_to :user
 
   validates :title, presence: true
-  validates :opinion,  presence: true, length: { maximum: 20 }
+  validates :body,  presence: true, length: { maximum: 20 }
 
 end
