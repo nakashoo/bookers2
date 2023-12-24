1af46882019547073f5e84386aaa9125dc812ce3
NakanishiShoko <129033329+nakashoo@users.noreply.github.com>
2023-12-24 02:37:49 +0000

    <add>nameカラムの一意性保証の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :introduction, length: { maximum: 50 }
-  validates :name, length: { maximum: 20 }
+  validates :name, uniqueness: true, length: { maximum: 20 }
 
   has_many :books, dependent: :destroy
 
